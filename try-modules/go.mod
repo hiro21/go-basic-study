e6f69045d39865de8591078064c5826421d3cbb3
hiroyuki-seto <hiswing8@gmail.com>
2021-12-17 21:29:15 +0900

    mod module add version2

--- try-modules/go.mod
+++ try-modules/go.mod
@@ -2,4 +2,7 @@ module try-module
 
 go 1.17
 
-require github.com/tenntenn/greeting v1.0.0
+require (
+	github.com/tenntenn/greeting v1.0.0
+	github.com/tenntenn/greeting/v2 v2.2.1
+)
--- try-modules/use-module.go
+++ try-modules/use-module.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/tenntenn/greeting"
+	greeting2 "github.com/tenntenn/greeting/v2"
 )
 
 func main() {
 	result := greeting.Do()
 	println(result)
+
+	g2 := greeting2.Do(time.Now())
+	println(g2)
 }
