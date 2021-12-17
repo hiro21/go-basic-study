a1d417edf30da0a3635f622ae65bbf0e4d83e885
hiroyuki-seto <hiswing8@gmail.com>
2021-12-17 21:00:14 +0900

    add module

---
+++ try-modules/go.mod
@@ -0,0 +1,5 @@
+module try-module
+
+go 1.17
+
+require github.com/tenntenn/greeting v1.0.0
---
+++ try-modules/use-module.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/tenntenn/greeting"
+)
+
+func main() {
+	result := greeting.Do()
+	println(result)
+}
