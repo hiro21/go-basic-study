ea78dc13bbea578cfa5be4a32bb7d8136c1dfb27
hiroyuki-seto <hiswing8@gmail.com>
2021-12-24 18:54:41 +0900

    不要なロジックを削除（osを消す)

--- try-cat-command/cat-command.go
+++ try-cat-command/cat-command.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 )
 
 /**
@@ -13,18 +12,6 @@ catコマンドの実装
 -nを指定された場合に行番号をつけて表示する。
 */
 func main() {
-	args := os.Args
-
-	argsLength := len(args)
-	if argsLength > 1 {
-		for i := 1; i < argsLength; i++ {
-			fmt.Println(args[i])
-		}
-	} else {
-		fmt.Println("no setting args")
-	}
-	fmt.Println("finished")
-
 	// -n取得のレシピ（後で最適化する）
 	nFlag := flag.Bool("n", false, "trueの場合にnumberを出力するフラグ")
 	flag.Parse()
@@ -32,8 +19,6 @@ func main() {
 
 	// フラグを覗いた引数を取得するレシピ
 	excludeFlag := flag.Args()
-	fmt.Println("flag.args", excludeFlag)
-	fmt.Println("flag.args.len", len(excludeFlag))
 	for i := 0; i < len(excludeFlag); i++ {
 		fmt.Println("excludeArgs", excludeFlag[i])
 	}
