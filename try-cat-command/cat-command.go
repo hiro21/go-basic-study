339287cc722776a19ab0c8c9c10423ad3c28a51f
hiroyuki-seto <hiswing8@gmail.com>
2021-12-24 09:51:32 +0900

    get n flag

--- try-cat-command/cat-command.go
+++ try-cat-command/cat-command.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -17,4 +18,9 @@ func main() {
 		fmt.Println("no setting args")
 	}
 	fmt.Println("finished")
+
+	// -n取得のレシピ（後で最適化する）
+	nFlag := flag.Bool("n", false, "trueの場合にnumberを出力するフラグ")
+	flag.Parse()
+	fmt.Println(*nFlag)
 }
